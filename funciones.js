9f38c45ee607bf52664e703af60ef397ec0d3211
Jhoel777ar <160800103+Jhoel777ar@users.noreply.github.com>
2024-09-05 20:40:50 -0400

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -5,25 +5,33 @@ document.addEventListener('DOMContentLoaded', () => {
             if (!startTimestamp) startTimestamp = timestamp;
             const progress = Math.min((timestamp - startTimestamp) / duration, 1);
             const currentValue = Math.floor(progress * (end - start) + start);
-            element.textContent = `+${currentValue}`; 
+            element.textContent = `+${currentValue}`;
             if (progress < 1) {
                 window.requestAnimationFrame(step);
             } else {
-                element.textContent = `+${end}`; 
+                element.textContent = `+${end}`;
             }
         };
         window.requestAnimationFrame(step);
     }
+
     function startCounting() {
         const visitorCountElement = document.getElementById('visitor-count');
-        const storedVisitorCount = parseInt(localStorage.getItem('visitorCount')) || 0;
-        animateCount(visitorCountElement, 0, storedVisitorCount, 2000);
+        let storedVisitorCount = parseInt(localStorage.getItem('visitorCount')) || 2220;
+        
+        // Animate from 1 to 2220 if it's the first visit or if the stored count is less than 2220
+        if (storedVisitorCount <= 2220) {
+            animateCount(visitorCountElement, 1, 2220, 2000);
+        } else {
+            // Animate from the previous stored count to the new count
+            animateCount(visitorCountElement, 2220, storedVisitorCount, 2000);
+        }
+
         const projectCountElement = document.getElementById('project-count');
         const projectLimit = 10;
         animateCount(projectCountElement, 0, projectLimit, 2000);
     }
-    startCounting();
-    setInterval(startCounting, 3000);
+
     function updateTime() {
         const now = new Date();
         const hours = now.getHours().toString().padStart(2, '0');
@@ -32,15 +40,19 @@ document.addEventListener('DOMContentLoaded', () => {
         const timeString = `${hours}:${minutes}:${seconds}`;
         document.getElementById('time').textContent = timeString;
     }
-    setInterval(updateTime, 1000);
+
     const referrer = document.referrer || 'Página no disponible';
     const referrerDomain = new URL(referrer).hostname;
     document.getElementById('address').textContent = referrerDomain;
 
     if (!sessionStorage.getItem('pageVisited')) {
-        let visitorCount = parseInt(localStorage.getItem('visitorCount')) || 0;
+        let visitorCount = parseInt(localStorage.getItem('visitorCount')) || 2220;
         visitorCount++;
         localStorage.setItem('visitorCount', visitorCount);
         sessionStorage.setItem('pageVisited', 'true');
     }
+
+    startCounting();
+    setInterval(startCounting, 3000);
+    setInterval(updateTime, 1000);
 });
