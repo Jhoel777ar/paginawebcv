2cbd3a9a4221d1fdeeb998c41bcfae53110db260
Jhoel777ar <160800103+Jhoel777ar@users.noreply.github.com>
2025-02-17 15:49:30 -0400

    Update arkdev.js

--- arkdev.js
+++ arkdev.js
@@ -1,6 +1,19 @@
-window.ArkDev = {
-    version: "1.0.0",
-    platform: "Ark Dev Commerce",
-    website: "https://arkdev.com",
-};
-console.log("Ark Dev Commerce loaded!");
+(function () {
+    const ArkDev = {
+        version: "4.0.0",
+        platform: "Ark Dev | Ecommerce Page Web",
+        website: "https://arkdev.com",
+    };
+    Object.defineProperty(window, "ArkDev", {
+        get() {
+            return ArkDev;
+        },
+        configurable: false,
+        enumerable: false,
+    });
+    console.log(
+        "%c Ark Dev | Ecommerce Page Web 4.0 %c https://arkdev.com ",
+        "color: white; background: #000; padding: 5px; font-size: 14px; font-weight: bold;",
+        "color: blue; text-decoration: underline; font-size: 14px;"
+    );
+})();
