165bda83c8a47edcbf402eda8890887420c9691e
Jhoel777ar <160800103+Jhoel777ar@users.noreply.github.com>
2025-02-17 15:54:49 -0400

    Update arkdev.js

--- arkdev.js
+++ arkdev.js
@@ -14,7 +14,6 @@
     });
 
     setTimeout(() => {
-        console.clear();
         console.log(
             "%c Ark Dev | Ecommerce Page Web 4.0 %c🌐 https://arkdev.pages.dev ",
             "color: #fff; background: #000; padding: 8px; font-size: 14px; font-weight: bold; border-radius: 5px;",
